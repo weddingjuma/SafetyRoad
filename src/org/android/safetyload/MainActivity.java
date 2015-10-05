b01b782663cbb8fadc5e812ef6c8764b1dd2cf79
sodam <kimsd16@naver.com>
2015-10-06 01:58:39 +0900

    modified mainActivity

--- src/org/android/safetyload/MainActivity.java
+++ src/org/android/safetyload/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		
 		startActivity(new Intent(this, SplashActivity.class));
