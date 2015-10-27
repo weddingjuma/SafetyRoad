43101d53e145695c7fb0adb12c74434a9bfa4e0a
choijinsung <bugslife102401@gmail.com>
2015-10-27 17:23:31 +0900

    modify UI of a layout for searching location

--- src/org/android/safetyroad/LocateSearchActivity.java
+++ src/org/android/safetyroad/LocateSearchActivity.java
@@ -1,19 +1,36 @@
 package org.android.safetyroad;
 
-import org.android.safetyroad.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import com.skp.Tmap.TMapView;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.Window;
-import android.widget.RelativeLayout;
+import java.util.ArrayList;
 
 public class LocateSearchActivity extends Activity {
 
 	public static final String APP_KEY = "62305c74-edf5-3198-bdce-ab26eced4be6";
 	private RelativeLayout locateSearchMap;
+        private EditText inputLocation;
+        private Spinner inputLocationSpinner;
+        private ListView searchListView;
+        static ArrayList<String> searchList = new ArrayList<String>();
+
+        static {
+          searchList.add("현재 위치");
+          searchList.add("검색 결과 1");
+          searchList.add("검색 결과 2");
+          searchList.add("검색 결과 3");
+          searchList.add("검색 결과 4");
+          searchList.add("검색 결과 5");
+        }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +38,25 @@ public class LocateSearchActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_locatesearch);
 
-		// Tmap 생성 및 설정
+                inputLocation = (EditText) findViewById(R.id.inputLocation);
+                inputLocationSpinner = (Spinner) findViewById(R.id.inputLocationSpinner);
+
+                searchListView = (ListView) findViewById(R.id.searchList);
+
+                ArrayAdapter<String>
+                searchListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, searchList);
+                searchListView.setAdapter(searchListAdapter);
+
+          // Tmap             
 		TMapView tmap = new TMapView(this);
 		tmap.setLanguage(TMapView.LANGUAGE_KOREAN);
 		tmap.setIconVisibility(true);
 		tmap.setZoomLevel(10);
 		tmap.setMapType(TMapView.MAPTYPE_STANDARD);
-		// 목적지를 보여주는 레이아웃에 tmap을 달아준다
+		//               ?     ??    tmap    ?  ? 
 		locateSearchMap = (RelativeLayout) findViewById(R.id.locateSearchMap);
 		locateSearchMap.addView(tmap);
-		// tmap 개인키 설정
+		// tmap     ?     
 		tmap.setSKPMapApiKey(APP_KEY);
 	}
 
