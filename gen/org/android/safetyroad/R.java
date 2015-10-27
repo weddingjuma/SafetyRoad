0185c59619a710a8e3b07e38a2d185674e942378
sodam <kimsd16@naver.com>
2015-10-27 12:50:00 +0900

    based UI

--- gen/org/android/safetyroad/R.java
+++ gen/org/android/safetyroad/R.java
@@ -22,18 +22,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_background=0x7f020001;
+        public static final int splash_image=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int arriveEntry=0x7f080006;
-        public static final int departureEntry=0x7f080005;
-        public static final int entireMap=0x7f080000;
-        public static final int locateSearchMap=0x7f080002;
-        public static final int mainTopLayout=0x7f080003;
-        public static final int routeMap=0x7f080008;
-        public static final int routeSearchBtn=0x7f080001;
-        public static final int searchBtn=0x7f080007;
-        public static final int settingBtn=0x7f080004;
+        public static final int Logo=0x7f080002;
+        public static final int action_settings=0x7f08000c;
+        public static final int arriveEntry=0x7f080009;
+        public static final int backBtn=0x7f080001;
+        public static final int departureEntry=0x7f080008;
+        public static final int entireMap=0x7f080004;
+        public static final int locateSearchMap=0x7f080006;
+        public static final int mainTopLayout=0x7f080007;
+        public static final int main_bar=0x7f080000;
+        public static final int routeMap=0x7f08000b;
+        public static final int routeSearchBtn=0x7f080005;
+        public static final int searchBtn=0x7f08000a;
+        public static final int settingBtn=0x7f080003;
     }
     public static final class layout {
         public static final int activity_entiremap=0x7f030000;
--- src/org/android/safetyroad/MainActivity.java
+++ src/org/android/safetyroad/MainActivity.java
@@ -1,16 +1,15 @@
 package org.android.safetyroad;
 
-import org.android.safetyroad.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -27,7 +26,7 @@ public class MainActivity extends Activity {
 		startActivity(new Intent(this, SplashActivity.class));
 		
 		Button searchBtn = (Button) findViewById(R.id.searchBtn);
-		Button settingBtn = (Button) findViewById(R.id.settingBtn);
+		ImageView settingBtn = (ImageView) findViewById(R.id.settingBtn);
 		
 		EditText departureEntry = (EditText) findViewById(R.id.departureEntry);
 		EditText arriveEntry = (EditText) findViewById(R.id.arriveEntry);
