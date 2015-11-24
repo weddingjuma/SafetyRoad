bdb2ab5892171a1db2d6ad28e62c1e87b6fa3735
sodam <kimsd16@naver.com>
2015-11-24 14:24:59 +0900

    access db and implement cctvMark

--- src/org/android/safetyroad/EntireMapActivity.java
+++ src/org/android/safetyroad/EntireMapActivity.java
@@ -1,6 +1,16 @@
 package org.android.safetyroad;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -19,6 +29,7 @@ import android.widget.ImageButton;
 
 import com.skp.Tmap.TMapData;
 import com.skp.Tmap.TMapData.TMapPathType;
+import com.skp.Tmap.TMapMarkerItem;
 import com.skp.Tmap.TMapPoint;
 import com.skp.Tmap.TMapPolyLine;
 import com.skp.Tmap.TMapView;
@@ -34,6 +45,10 @@ public class EntireMapActivity extends Activity {
 	private TMapPoint endPoint;
 	private ImageButton settingBtn;
 	private ImageButton backBtn;
+	
+	private ArrayList<TMapPoint> posOfCCTV;
+	
+	private static String url = "http://openapi.seoul.go.kr:8088/516943625268733134394857784576/json/TB_GC_VVTV_INFO_ID01/1/400/";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +57,8 @@ public class EntireMapActivity extends Activity {
 		setContentView(R.layout.activity_entiremap);
 		
 		tmap = (TMapView) findViewById(R.id.entireMapLayout);
-		settingBtn = (ImageButton) findViewById(R.id.settingBtn);
-		backBtn = (ImageButton) findViewById(R.id.backBtn);
 		
+		settingBtn = (ImageButton) findViewById(R.id.settingBtn);
 		settingBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -54,12 +68,15 @@ public class EntireMapActivity extends Activity {
 			}
 		});
 		
+		backBtn = (ImageButton) findViewById(R.id.backBtn);
 		backBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				finish();
 			}
-		});
+		});		
+
+		new setPointOfCCTV().execute();
 		
 		new MapRegisterTask().execute("");		
 		
@@ -68,9 +85,8 @@ public class EntireMapActivity extends Activity {
 		endPoint = new TMapPoint(37.570101477782934, 126.9920825958251);
 		
 		new RouteSearchTask().execute(startPoint,endPoint);
-				
+						
 		Button routeSearchBtn = (Button) findViewById(R.id.routeSearchBtn);		
-
 		routeSearchBtn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -78,6 +94,133 @@ public class EntireMapActivity extends Activity {
 				startActivityForResult(intent, REQUEST_CODE_ROUTE);
 			}
 		});		
+		
+	}
+	
+	private class setPointOfCCTV extends AsyncTask<String, Void, JSONArray> {
+			
+        @Override
+        protected JSONArray doInBackground(String... strs) {
+        	
+        	String jsonPage = getStringFromUrl(url);
+        	        	
+        	JSONObject json = null;
+			try {
+				json = new JSONObject(jsonPage);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+    		JSONObject json2 = null;
+			try {
+				json2 = new JSONObject(json.getString("TB_GC_VVTV_INFO_ID01"));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    		
+    		JSONArray jArr = null;
+			try {
+				jArr = json2.getJSONArray("row");
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}        	
+        	return jArr;
+        }
+        @Override
+        protected void onPostExecute(JSONArray result) {
+        	
+        	posOfCCTV = new ArrayList<TMapPoint>();	        	
+        	
+        	for(int i=0; i<result.length(); i++){
+        		
+        		JSONObject insideObject = null;
+        		TMapPoint tmp = new TMapPoint(0, 0);
+        		
+        		try {
+					insideObject = result.getJSONObject(i);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				};
+				
+				try {
+					tmp.setLatitude(insideObject.getDouble("GC_MAPX"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				try {
+					tmp.setLongitude(insideObject.getDouble("GC_MAPY"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+				posOfCCTV.add(tmp);        	
+        	}
+        	
+/*        	for(TMapPoint t : posOfCCTV){
+        		System.out.print("latitu : "+t.getLatitude()+" long : "+t.getLongitude()+"\n");
+           	}*/
+
+        	setMarkerOfCCTV();
+        } 
+    }
+		
+	public String getStringFromUrl(String pUrl){
+		
+		BufferedReader bufreader=null;
+		HttpURLConnection urlConnection = null;
+		
+		StringBuffer page=new StringBuffer();
+		
+		try {
+			  
+        	URL url= new URL(pUrl);
+        	urlConnection = (HttpURLConnection) url.openConnection();
+        	InputStream contentStream = urlConnection.getInputStream();			
+        	
+			bufreader = new BufferedReader(new InputStreamReader(contentStream,"UTF-8"));						
+			String line = null;				
+					
+			while((line = bufreader.readLine())!=null){
+					Log.d("line:",line);
+					page.append(line);				
+			}			
+		
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}finally{
+			try {
+				bufreader.close();
+				urlConnection.disconnect();
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}					
+		}
+		
+		return page.toString();
+	}
+
+	
+	public void setMarkerOfCCTV(){
+		//정보 긁어와서 넣고
+		//마커로 셋팅하면되나?	
+		
+		for(int i=0; i<posOfCCTV.size(); i++){
+			TMapMarkerItem tItem = new TMapMarkerItem();
+			tItem.setTMapPoint(posOfCCTV.get(i));
+			tItem.setName("cctv"+i);
+			tItem.setVisible(TMapMarkerItem.VISIBLE);
+			Bitmap bm = ((BitmapDrawable)getResources().getDrawable(R.drawable.ic_launcher)).getBitmap();
+			tItem.setIcon(bm);
+			
+			tItem.setPosition(0.5f, 1.0f);
+			tmap.addMarkerItem("testid"+i, tItem);
+		}
 		
 	}
 		
